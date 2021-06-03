777ab16ca9575534efbfe84ef62d9501aee3cd79
Wouter van den Heuvel <exo_00@hotmail.com>
2021-06-03 23:25:53 +0200

    added buttons

--- src/App/ButtonBar/ButtonBar.tsx
+++ src/App/ButtonBar/ButtonBar.tsx
@@ -1,5 +1,7 @@
 import './styles/buttonBar.scss';
 import { ReactComponent as FlipIcon } from './images/flip-24px.svg';
+import { ReactComponent as ThumbsDownIcon } from './images/thumb-down-24px.svg';
+import { ReactComponent as ThumbsUpIcon } from './images/thumb-up-24px.svg';
 
 interface Props {
 }
@@ -8,13 +10,13 @@ const ButtonBar = (props: Props) => {
   return (
     <div className="button-bar">
       <button className="button-circle" >
+        <ThumbsDownIcon />
+      </button>
+      <button className="button-circle button-small" >
         <FlipIcon />
       </button>
       <button className="button-circle" >
-        <FlipIcon />
-      </button>
-      <button className="button-circle" >
-        <FlipIcon />
+        <ThumbsUpIcon />
       </button>
     </div>
   )
