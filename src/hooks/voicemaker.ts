f9e669a00cb51232c302e23e02237c69cac9da37
Wouter van den Heuvel <exo_00@hotmail.com>
2022-02-21 22:58:36 +0100

    play the sound using howl

--- src/hooks/voicemaker.ts
+++ src/hooks/voicemaker.ts
@@ -1,31 +1,46 @@
+import axios from "axios";
+import { Howl } from "howler";
 import { useCallback } from "react";
+
+type Response = {
+  success: boolean
+  path: string
+}
 
 const useVoicemaker = (lang: string) => {
   const speak = useCallback(async (text: string) => {
-    const headers = new Headers({
+    const headers = {
       'content-type': 'application/json',
       'Authorization': `Bearer ${process.env.REACT_APP_VOICEMAKER_API_KEY}`,
-    })
-    const body = {
-      "Engine": "neural",
-      "VoiceId": "ai3-Jony",
-      "LanguageCode": "en-US",
-      "Text": "Welcome to the Air.",
-      "OutputFormat": "mp3",
-      "SampleRate": "48000",
-      "Effect": "default",
-      "MasterSpeed": "0",
-      "MasterVolume": "0",
-      "MasterPitch": "0"
-      // Engine: "neural",
-      // VoiceId: "ai3-Jony",
-      // LanguageCode: lang,
-      // Text: text
     }
-    console.log(JSON.stringify(body))
-    const response = await sendRequest('https://developer.voicemaker.in/voice/api', 'POST', body, headers);
-    const json = await response.text();
-    console.log(json)
+    const data = {
+      // "Engine": "neural",
+      // "VoiceId": "ai3-Jony",
+      // "LanguageCode": "en-US",
+      // "Text": "Welcome to the Air.",
+      Engine: "neural",
+      VoiceId: "ai3-Jony",
+      LanguageCode: lang,
+      Text: text,
+      OutputFormat: "mp3",
+      SampleRate: "48000",
+      Effect: "default",
+      MasterSpeed: "0",
+      MasterVolume: "0",
+      MasterPitch: "0"
+    }
+    // console.log(JSON.stringify(body))
+    const response = await axios.post<Response>('https://developer.voicemaker.in/voice/api', data, {
+      headers: headers
+    })
+    if (response.data.success) {
+      const { path } = response.data;
+      const howl = new Howl({
+        src: [path]
+      });
+
+      howl.play();
+    }
   }, [lang]);
 
   return {
@@ -35,13 +50,13 @@ const useVoicemaker = (lang: string) => {
 
 export default useVoicemaker;
 
-const sendRequest = (url: string, method: string, body: unknown, headers: Headers) => {
-  const options = {
-      method: method,
-      headers,
-      mode: 'no-cors' as RequestMode,
-      body: JSON.stringify(body)
-  };
+// const sendRequest = (url: string, method: string, body: unknown, headers: Headers) => {
+//   const options = {
+//       method: method,
+//       headers,
+//       mode: 'no-cors' as RequestMode,
+//       body: JSON.stringify(body)
+//   };
 
-  return fetch(url, options);
-}
+//   return fetch(url, options);
+// }
