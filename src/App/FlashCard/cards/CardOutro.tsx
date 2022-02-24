f7c12f379ebf02b349f4edd3d60916e237d767e0
Wouter van den Heuvel <exo_00@hotmail.com>
2022-02-24 19:35:08 +0100

    processed kritis requests

--- src/App/FlashCard/cards/CardOutro.tsx
+++ src/App/FlashCard/cards/CardOutro.tsx
@@ -1,12 +1,10 @@
-import React, { createRef, forwardRef, useImperativeHandle, useRef, useState } from "react"
-import { Swipeable, direction } from "../Swipeable";
-import {Howl} from 'howler';
+import { useMemo } from 'react';
 import { useTranslationStore } from 'stores/translations';
 import './../styles/flashCard.scss';
 
 type Props = {
   mistakes: number
-} 
+}
 
 
 
@@ -14,11 +12,15 @@ const CardOutro = (props: Props) => {
   const { mistakes } = props;
   const translations = useTranslationStore();
 
-  const mistakeText = translations.getTextRaw("outro-description")?.replace("{0}", `${mistakes}`);
+  console.log(translations.getText('outro-description-flawless'))
+  const mistakeText = useMemo(() => {
+    if (mistakes === 0 && !!translations.getText('outro-description-flawless')) return translations.getText('outro-description-flawless');
+    return translations.getTextRaw("outro-description")?.replace("{0}", `${mistakes}`);
+  }, [mistakes, translations]);
   return (
     <div className='flash-card-wrapper'>
       <div className="card card-outro">
-        <h2>          
+        <h2>
           {translations.getText("outro-header")}
         </h2>
         <div className="outro-description">
