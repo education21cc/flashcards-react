602ace305df46e042c0e999c344fd42e669fed9e
Wouter van den Heuvel <exo_00@hotmail.com>
2022-04-14 00:29:20 +0200

    cleanup

--- src/App/Instructions/InstructionPageComponent.tsx
+++ src/App/Instructions/InstructionPageComponent.tsx
@@ -1,12 +1,5 @@
-import ReactMarkdown from "react-markdown";
-import directive from 'remark-directive';
-import { Instruction } from "data/Instruction"
-import { useTranslationStore } from "stores/translations"
-import { ReactComponent as NextIcon } from './../../images/icons/next-24px.svg';
-import { reactMarkdownRemarkDirective } from "markdown/reactMarkdownDirective";
 import './styles/instructionPage.scss'
-import { lazy, Suspense, useMemo } from "react";
-
+import { lazy, Suspense } from "react";
 interface Props {
   componentPath: string
 }
@@ -14,18 +7,8 @@ interface Props {
 // Lazy loads a react component
 const InstructionPageComponent = (props: Props) => {
   const { componentPath } = props
-  const translations = useTranslationStore();
 
-  const OtherComponent = lazy(() => import('instructionPages/swiggy/Instruction'));
-  // const { isLoading, result: Components } = useLazy(
-  //   // Preserves identity of "imports" so it can be safely add as a dependency of useEffect
-  //   // inside useLazy
-  //   // useMemo(() => [() => import('instructionPages/swiggy/Instruction')], []),
-  //   useMemo(() => [() => import(componentPath)], [componentPath]),
-  //   true,
-  // );
-
-  // const TextComponent = result;
+  const OtherComponent = lazy(() => import(`instructionPages/${componentPath}`));
 
   return (
     <>
