82729db33a69b8914470b25721b8ec385a7025da
Wouter van den Heuvel <exo_00@hotmail.com>
2022-04-16 11:31:03 +0200

    Updates

---
+++ static/js/5.1c25e3d0.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpflashcards-react"]=this["webpackJsonpflashcards-react"]||[]).push([[5],{86:function(s,a,c){}}]);
+//# sourceMappingURL=5.1c25e3d0.chunk.js.map
--- static/js/5.45d5f9fd.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpflashcards-react"]=this["webpackJsonpflashcards-react"]||[]).push([[5],{85:function(s,a,c){}}]);
-//# sourceMappingURL=5.45d5f9fd.chunk.js.map
